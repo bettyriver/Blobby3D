184b01d4fa7218350d9b2ccaa244c251da1ee7d8
mathewvaridel <mathewvaridel87@gmail.com>
2018-10-03 19:21:03 +1000

    Removed using namespace.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 
 #include "DNest4/code/DNest4.h"
+
 #include "Data.h"
 #include "MyModel.h"
 
-using namespace std;
-using namespace DNest4;
+// using namespace DNest4;
 
-int main(int argc, char** argv) { 
+int main(int argc, char** argv) {
   // Get command line options
-  CommandLineOptions options(argc, argv);
-  
+  DNest4::CommandLineOptions options(argc, argv);
+
   // Get model specific options file
   const char* moptions_file;
   if (options.get_config_file() == "") {
-    cerr<<"# ERROR: No model options file provided. "
-	<<"Specify model options file as a command line option "
-	<<"using -f <MODEL_OPTIONS_FILE>."<<std::endl;
+    std::cerr
+      <<"# ERROR: No model options file provided. "
+	    <<"Specify model options file as a command line option "
+	    <<"using -f <MODEL_OPTIONS_FILE>."
+      <<std::endl;
     exit(0);
   } else {
     moptions_file = options.get_config_file().c_str();
@@ -26,7 +28,7 @@ int main(int argc, char** argv) {
   Data::get_instance().load(moptions_file);
 
   // Setup and run sampler
-  Sampler<MyModel> sampler = setup<MyModel>(options);
+  DNest4::Sampler<MyModel> sampler = DNest4::setup<MyModel>(options);
   sampler.run();
 
   return 0;
