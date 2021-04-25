a95ce1b787516c221fd4586c0d64320b29286ecb
Mathew Varidel <mathewvaridel@192-168-1-102.tpgi.com.au>
2021-04-25 16:23:28 +1000

    Removing comparison plot. Still a work in progress.

--- examples/485885/post.py
+++ examples/485885/post.py
@@ -40,7 +40,7 @@ sample = 0
 
 # Plot maps for sample
 fig, ax = plt.subplots(1, 4)
-map_names = ['FLUX0', 'FLUX1', 'V', 'VDISP']
+map_names = ['FLUåX0', 'FLUX1', 'V', 'VDISP']
 for i in range(4):
     ax[i].set_title(map_names[i])
     ax[i].imshow(b3d.maps[sample, i], interpolation='nearest', origin='lower')
@@ -71,47 +71,3 @@ b3d.blob_param.loc[sample, ['FLUX0', 'FLUX1']].hist(bins=30)
 # Marginalised samples for the mean and standard deviation of the flux for the
 # first emission line
 b3d.global_param[['FLUX0MU', 'FLUX0SD']].hist()
-
-# # Construct a comparison between Blobby3D and a single component fit
-# sm = b3dtk.SpectralModel(
-#         lines=[[6562.81], [6583.1, 6548.1, 0.3333]],
-#         lsf_fwhm=1.59
-#         )
-
-# wave = np.linspace(
-#         b3d.r_lim[0] + 0.5*b3d.dr,
-#         b3d.r_lim[1] - 0.5*b3d.dr,
-#         b3d.naxis[2])
-
-# # Fitting convolved model and data cubes with Gaussians per spaxel
-# fit_conv, fit_conv_err = sm.fit_cube(wave, b3d.con_cubes[sample])
-# fit_data, fit_data_err = sm.fit_cube(wave, b3d.data)
-
-# # Make the plot
-# fig, ax = b3d.setup_comparison_maps()
-
-# # Add sample model, convolved model and data
-# b3d.add_comparison_maps(b3d.maps[sample], ax, col=0, log_flux=True)
-# b3d.add_comparison_maps(fit_conv, ax, col=1, log_flux=True)
-# b3d.add_comparison_maps(fit_data, ax, col=2, log_flux=True)
-
-# b3d.update_comparison_clim(
-#     ax=ax[0][:3], cax=ax[0][3],
-#     pct=100.0, absolute=False)
-# b3d.update_comparison_clim(ax[1][:3], ax[1][3], pct=100.0, absolute=False)
-# b3d.update_comparison_clim(ax[2][:3], ax[2][3], pct=100.0, absolute=True)
-# b3d.update_comparison_clim(ax[3][:3], ax[3][3], pct=100.0, absolute=False)
-
-# # Add residuals
-# residuals = fit_conv - fit_data
-# residuals[0,] = np.log10(fit_conv[0]) - np.log10(fit_data[0])
-# residuals[1] = np.log10(fit_conv[0]) - np.log10(fit_data[0])
-# b3d.add_comparison_residuals(residuals, ax, col=4)
-
-# mask = np.ma.masked_invalid(fit_data[0]).mask
-# b3d.update_comparison_mask(mask, (ax[0][0], ax[1][0], ax[2][0], ax[3][0]))
-
-# b3d.update_comparison_clim(ax[0][4], ax[0][5], pct=100.0, absolute=True)
-# b3d.update_comparison_clim(ax[1][4], ax[1][5], pct=100.0, absolute=True)
-# b3d.update_comparison_clim(ax[2][4], ax[2][5], pct=100.0, absolute=True)
-# b3d.update_comparison_clim(ax[3][4], ax[3][5], pct=100.0, absolute=True)
